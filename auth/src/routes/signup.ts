d6880585f0d5bb0f8e6690c9aafa04f595fad634
william <william.del.rosario@iceshop.nl>
2020-06-13 03:55:53 +0200

    User Creation

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,7 +1,7 @@
 import express, { Request, Response } from 'express';
 import {body, validationResult} from "express-validator";
 import {RequestValidationError} from "../errors/request-validation-error";
-import {DatabaseConnectionError} from "../errors/database-connection-error";
+import {User} from "../model/user";
 
 const router = express.Router();
 
@@ -22,10 +22,19 @@ router.post(
             throw new RequestValidationError(errors.array());
         }
 
-        console.log('Creating a user...');
-        throw new DatabaseConnectionError();
+        const { email, password } = req.body;
 
-        res.send({});
+        const existingUser = await User.findOne({ email });
+
+        if (existingUser) {
+            console.log('Email in use');
+            return res.send({});
+        }
+
+        const user = User.build({ email, password });
+        await user.save();
+
+        res.status(201).send(user)
 });
 
 export { router as signupRouter }
