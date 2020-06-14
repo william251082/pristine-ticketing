49b66edd72312b268111b79370644d1fda5d45c8
william <william.del.rosario@iceshop.nl>
2020-06-14 08:06:00 +0200

    Sign In Logic

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,6 +1,10 @@
 import express, { Request, Response } from 'express';
 import {body} from "express-validator";
 import {validateRequest} from "../middlewares/validate-request";
+import {User} from "../model/user";
+import {BadRequestError} from "../errors/bad-request-error";
+import {Password} from "../services/password";
+import jwt from "jsonwebtoken";
 
 const router = express.Router();
 
@@ -16,8 +20,34 @@ router.post(
         .withMessage('Password must be between 4 and 20 characters')
     ],
     validateRequest,
-    (req: Request, res: Response) => {
+    async (req: Request, res: Response) => {
+        const { email, password } = req.body;
+        const existingUser = await User.findOne({ email });
 
+        if (!existingUser) {
+            throw new BadRequestError('Invalid credentials');
+        }
+
+        const passwordsMatch = await Password.compare(
+            existingUser.password,
+            password
+        );
+        if (!passwordsMatch) {
+            throw new BadRequestError('Invalid credentials');
+        }
+
+        // Generate jwt
+        const userJwt = jwt.sign({
+            id: existingUser.id,
+            email: existingUser.email
+        }, process.env.JWT_KEY!);
+
+        // Store it on session object
+        req.session = {
+            jwt: userJwt
+        };
+
+        res.status(201).send(existingUser)
     }
 );
 
