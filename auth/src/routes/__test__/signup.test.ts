33a3fb0421a8522ade8da5e7fad3ea6435ee1a11
william <william.del.rosario@iceshop.nl>
2020-06-14 13:19:10 +0200

    Requiring Unique Emails

--- auth/src/routes/__test__/signup.test.ts
+++ auth/src/routes/__test__/signup.test.ts
@@ -56,3 +56,21 @@ it('returns on 400 on missing email or password', async () => {
         })
         .expect(400);
 });
+
+it('disallows duplicate emails', async () => {
+    await request(app)
+        .post('/api/users/signup')
+        .send({
+            email: 'test@test.com',
+            password: 'password'
+        })
+        .expect(201);
+
+    await request(app)
+        .post('/api/users/signup')
+        .send({
+            email: 'test@test.com',
+            password: 'password'
+        })
+        .expect(400);
+});
