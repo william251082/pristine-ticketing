2df1968388dc410a8d76ad346b99a80703027a65
william <william.del.rosario@iceshop.nl>
2020-06-14 11:14:58 +0200

    Augmenting Type Definitions

--- auth/src/middlewares/current-user.ts
+++ auth/src/middlewares/current-user.ts
@@ -1,6 +1,19 @@
 import {Request, Response, NextFunction} from 'express';
 import jwt from 'jsonwebtoken';
 
+interface UserPayload {
+    id: string,
+    email: string
+}
+
+declare global {
+    namespace Express {
+        interface Request {
+            currentUser?: UserPayload;
+        }
+    }
+}
+
 export const currentUser = (
     req: Request,
     res: Response,
@@ -11,8 +24,8 @@ export const currentUser = (
     }
 
     try {
-        const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!);
-        req.currentUser = payload;
+        req.currentUser = jwt.verify(req.session.jwt, process.env.JWT_KEY!) as UserPayload;
+        const payload = req.currentUser;
     } catch (err) {
 
     }
