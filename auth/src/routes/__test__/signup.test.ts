b02b384658ceff1e745a57d563eb93125f6f1d41
william <william.del.rosario@iceshop.nl>
2020-06-14 13:16:50 +0200

    Testing Invalid Input

--- auth/src/routes/__test__/signup.test.ts
+++ auth/src/routes/__test__/signup.test.ts
@@ -10,3 +10,49 @@ it('returns on 201 on successful signup', async () => {
         })
         .expect(201)
 });
+
+it('returns on 400 on an invalid email', async () => {
+    return request(app)
+        .post('/api/users/signup')
+        .send({
+            email: 'testtest.com',
+            password: 'password'
+        })
+        .expect(400);
+});
+
+it('returns on 400 on an invalid password', async () => {
+    return request(app)
+        .post('/api/users/signup')
+        .send({
+            email: 'test@test.com',
+            password: 'p'
+        })
+        .expect(400);
+});
+
+it('returns on 400 on an email and invalid password', async () => {
+    return request(app)
+        .post('/api/users/signup')
+        .send({
+            email: 'testtest.com',
+            password: 'p'
+        })
+        .expect(400);
+});
+
+it('returns on 400 on missing email or password', async () => {
+    await request(app)
+        .post('/api/users/signup')
+        .send({
+            email: 'test@test.com'
+        })
+        .expect(400);
+
+    await request(app)
+        .post('/api/users/signup')
+        .send({
+            password: 'password'
+        })
+        .expect(400);
+});
