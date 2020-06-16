8deae27b81d0ad90c9cbf09a921094d6ac767bed
william <william.del.rosario@iceshop.nl>
2020-06-16 13:08:00 +0200

    When is GetInitialProps Called?

--- client/pages/index.jsx
+++ client/pages/index.jsx
@@ -1,17 +1,19 @@
 import axios from "axios";
 
 const LandingPage = ({ color }) => {
-    console.log('I am the component', color);
-    // request on browser
-    axios.get('/api/users/currentuser');
+    // console.log('I am the component', color);
+    // // request on browser
+    // axios.get('/api/users/currentuser');
     return <h1>Index Page!!!!!!</h1>;
 };
 
-// LandingPage.getInitialProps = async () => {
-//     // request on server
-//     const response = await axios.get('/api/users/currentuser');
-//
-//     return response.data;
-// };
+LandingPage.getInitialProps = async () => {
+    // // request on server
+    // const response = await axios.get('/api/users/currentuser');
+    //
+    // return response.data;
+    console.log('getInitialProps executed');
+    return {};
+};
 
 export default  LandingPage;
