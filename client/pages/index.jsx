6eaa8e2bb4982fdc493355b31662f0f75ac7d104
william <william.del.rosario@iceshop.nl>
2020-06-16 12:31:47 +0200

    Overview on Server Side Rendering

--- client/pages/index.jsx
+++ client/pages/index.jsx
@@ -1,3 +1,14 @@
-export default () => {
+const LandingPage = ({ color }) => {
+    console.log('I am the component', color);
     return <h1>Index Page!!!!!!</h1>;
 };
+
+// nextjs will call getInitialProps  while attempting to render the app on the server
+// opportunity to fetch some data that the component needs
+LandingPage.getInitialProps = () => {
+    console.log('I am on the server!');
+
+    return { color: 'red' }
+};
+
+export default  LandingPage;
