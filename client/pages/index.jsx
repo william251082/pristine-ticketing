f03fa7716f97a9b864fd09d74ca0e61bfb931126
william <william.del.rosario@iceshop.nl>
2020-06-16 12:37:19 +0200

    Fetching Data During SSR

--- client/pages/index.jsx
+++ client/pages/index.jsx
@@ -1,14 +1,17 @@
+import axios from "axios";
+
 const LandingPage = ({ color }) => {
     console.log('I am the component', color);
+    // request on browser
+    axios.get('/api/users/currentuser');
     return <h1>Index Page!!!!!!</h1>;
 };
 
-// nextjs will call getInitialProps  while attempting to render the app on the server
-// opportunity to fetch some data that the component needs
-LandingPage.getInitialProps = () => {
-    console.log('I am on the server!');
+LandingPage.getInitialProps = async () => {
+    // request on server
+    const response = await axios.get('/api/users/currentuser');
 
-    return { color: 'red' }
+    return response.data;
 };
 
 export default  LandingPage;
