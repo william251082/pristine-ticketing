9a67706a83478bf7f9a6a5a9dbbd977fa6211d81
william <william.del.rosario@iceshop.nl>
2020-06-19 15:21:21 +0200

    Specifying the Host
    Passing Through the Cookies

--- auth/src/routes/__test__/current-user.test.ts
+++ auth/src/routes/__test__/current-user.test.ts
@@ -17,8 +17,8 @@ it('response with error if not authenticated', async () => {
     const response = await request(app)
         .get('/api/users/currentuser')
         .send()
-        .expect(401);
+        .expect(200);
 
-    expect(response.body.errors[0].message).toEqual('Not Authorized');
+    expect(response.body.currentUser).toEqual(null);
 });
 
--- client/pages/index.jsx
+++ client/pages/index.jsx
@@ -1,22 +1,32 @@
 import axios from "axios";
 
-const LandingPage = ({ color }) => {
+const LandingPage = ({ currentUser }) => {
     // console.log('I am the component', color);
     // // request on browser
     // axios.get('/api/users/currentuser');
+    console.log('curr user', currentUser);
     return <h1>Index Page!!!!!!</h1>;
 };
 
-LandingPage.getInitialProps = async () => {
+LandingPage.getInitialProps = async ({ req }) => {
+    // console.log('headers', req.headers);
     if (typeof window === 'undefined') {
         // we are on the server
         // requests should be made to http://ingress-nginx.....
+        const { data } = await axios.get(
+           'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser', {
+                headers: req.headers
+            }
+        );
+        console.log('data', data);
+        return data;
     } else {
         // we are on the browser
         // request can be made with a base url of '
-    }
+        const { data } = await axios.get('/api/users/currentuser');
 
-    return {};
+        return data;
+    }
 };
 
 export default  LandingPage;
