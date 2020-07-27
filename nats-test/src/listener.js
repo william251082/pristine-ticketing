f5a1b7bc999f2ffe0d257441c5b12e9059cc61c8
william <william.del.rosario@iceshop.nl>
2020-07-27 09:16:42 +0200

    Listening For Data

--- nats-test/src/listener.js
+++ nats-test/src/listener.js
@@ -1,1 +1,17 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var node_nats_streaming_1 = __importDefault(require("node-nats-streaming"));
+console.clear();
+var stan = node_nats_streaming_1.default.connect('ticketing', '123', {
+    url: 'http://localhost:4222'
+});
+stan.on('connect', function () {
+    console.log('Listener connected to NATS');
+    var subscription = stan.subscribe('ticket:created');
+    subscription.on('message', function (msg) {
+        console.log('Message received');
+    });
+});
--- nats-test/src/publisher.js
+++ nats-test/src/publisher.js
@@ -4,6 +4,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 var node_nats_streaming_1 = __importDefault(require("node-nats-streaming"));
+console.clear();
 var stan = node_nats_streaming_1.default.connect('ticketing', 'abc', {
     url: 'http://localhost:4222'
 });
