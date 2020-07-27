d9af65a5bc2eab2c28d3fc150cabf7b564c1eabd
william <william.del.rosario@iceshop.nl>
2020-07-27 08:58:15 +0200

    Port-Forwarding with Kubectl

--- nats-test/src/listener.ts
+++
@@ -1,9 +0,0 @@
-import nats from 'node-nats-streaming';
-
-const stan = nats.connect('ticketing', 'abc', {
-    url: 'http://localhost:4222'
-});
-
-stan.on('connect', () => {
-    console.log('Publisher connected to NATS')
-});
--- nats-test/src/publisher.js
+++ nats-test/src/publisher.js
@@ -1,1 +1,12 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var node_nats_streaming_1 = __importDefault(require("node-nats-streaming"));
+var stan = node_nats_streaming_1.default.connect('ticketing', 'abc', {
+    url: 'http://localhost:4222'
+});
+stan.on('connect', function () {
+    console.log('Publisher connected to NATS');
+});
