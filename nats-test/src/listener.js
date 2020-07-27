3b9aa770ec84de6ecf77a1f503e19aa4d214262f
william <william.del.rosario@iceshop.nl>
2020-07-27 15:08:17 +0200

    Durable Subscriptions

--- nats-test/src/listener.js
+++ nats-test/src/listener.js
@@ -18,8 +18,9 @@ stan.on('connect', function () {
     var options = stan
         .subscriptionOptions()
         .setManualAckMode(true)
-        .setDeliverAllAvailable();
-    var subscription = stan.subscribe('ticket:created', options);
+        .setDeliverAllAvailable()
+        .setDurableName('accounting-service');
+    var subscription = stan.subscribe('ticket:created', 'queue-group-name', options);
     subscription.on('message', function (msg) {
         var data = msg.getData();
         if (typeof data === 'string') {
