b23b73cc0e7f37bdb38d1dd26b41da177f834fe2
william <william.del.rosario@iceshop.nl>
2020-07-28 01:02:06 +0200

    Singleton Implementation

--- tickets/src/nats-wrapper.ts
+++ tickets/src/nats-wrapper.ts
@@ -1,5 +1,21 @@
-class NatsWrapper {
+import nats, { Stan } from 'node-nats-streaming';
 
+class NatsWrapper {
+    private _client?: Stan;
+
+    connect(clusterId: string, clientId: string, url: string) {
+        this._client = nats.connect(clusterId, clientId, { url });
+
+        return new Promise((resolve, reject) => {
+                this._client!.on('connect', () => {
+                console.log('Connected NATS ');
+                resolve();
+            });
+                this._client!.on('error', () => {
+                    reject(err);
+                });
+        });
+    }
 }
 
 export const natsWrapper = new NatsWrapper();
