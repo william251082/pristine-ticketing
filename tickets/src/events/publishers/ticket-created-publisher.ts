16c192c0c1d159cbed97d3a3b69770c9eecf22b7
william <william.del.rosario@iceshop.nl>
2020-07-28 00:32:23 +0200

    More on Publishing

--- tickets/src/events/publishers/ticket-created-publisher.ts
+++ tickets/src/events/publishers/ticket-created-publisher.ts
@@ -3,5 +3,3 @@ import {Publisher, Subjects, TicketCreatedEvent} from "@iceshoptickets/common";
 export class TicketCreatedPublisher extends Publisher<TicketCreatedEvent> {
     subject: Subjects.TicketCreated = Subjects.TicketCreated
 }
-
-// new TicketCreatedPublisher(client).publish(ticket);
