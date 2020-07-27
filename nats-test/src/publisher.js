ed74eb5e1791bbe741789f7419a914136db6fc11
william <william.del.rosario@iceshop.nl>
2020-07-27 23:50:19 +0200

    Using the Custom Publisher

--- nats-test/src/publisher.js
+++ nats-test/src/publisher.js
@@ -4,18 +4,26 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 var node_nats_streaming_1 = __importDefault(require("node-nats-streaming"));
+var ticket_created_publisher_1 = require("./events/ticket-created-publisher");
 console.clear();
 var stan = node_nats_streaming_1.default.connect('ticketing', 'abc', {
     url: 'http://localhost:4222'
 });
 stan.on('connect', function () {
     console.log('Publisher connected to NATS');
-    var data = JSON.stringify({
+    var publisher = new ticket_created_publisher_1.TicketCreatedPublisher(stan);
+    publisher.publish({
         id: '123',
         title: 'concert',
-        price: 20,
+        price: 20
     });
-    stan.publish('ticket:created', data, function () {
-        console.log('Event Published');
-    });
+    // const data = JSON.stringify({
+    //     id: '123',
+    //     title: 'concert',
+    //     price: 20,
+    // });
+    //
+    // stan.publish('ticket:created', data, () => {
+    //     console.log('Event Published')
+    // });
 });
