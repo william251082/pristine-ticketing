a2b52acc679b536210a69487ef4af9d2d0350b47
william <william.del.rosario@iceshop.nl>
2020-07-28 09:46:00 +0200

    Ticket Update Publishing

--- tickets/src/routes/update.ts
+++ tickets/src/routes/update.ts
@@ -2,6 +2,8 @@ import express, { Request, Response } from 'express';
 import {NotAuthorizedError, NotFoundError, requireAuth, validateRequest} from "@iceshoptickets/common";
 import {Ticket} from "../models/ticket";
 import {body} from "express-validator";
+import {TicketUpdatedPublisher} from "../events/publishers/ticket-updated-publisher";
+import {natsWrapper} from "../nats-wrapper";
 
 const router = express.Router();
 
@@ -29,6 +31,12 @@ router.put('/api/tickets/:id', requireAuth, [
         price: req.body.price
     });
     await ticket.save();
+    new TicketUpdatedPublisher(natsWrapper.client).publish({
+        id: ticket.id,
+        title: ticket.title,
+        price: ticket.price,
+        userId: ticket.userId,
+    });
 
     res.send(ticket);
 });
