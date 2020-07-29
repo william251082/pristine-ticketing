5d38d1cac62383c489c53ea88e6066c71bf53f91
william <william.del.rosario@iceshop.nl>
2020-07-29 08:45:08 +0200

    Scaffolding the Orders Service

---
+++ orders/src/app.ts
@@ -0,0 +1,36 @@
+import express, {Request, Response} from 'express';
+import 'express-async-errors'
+import { json } from 'body-parser';
+import cookieSession from 'cookie-session';
+
+import { errorHandler, NotFoundError, currentUser } from "@iceshoptickets/common";
+
+import {createTicketRouter} from "./routes/new";
+import {showTicketRouter} from "./routes/show";
+import {indexTicketRouter} from "./routes";
+import {updateTicket} from "./routes/update";
+
+const app = express();
+// make sure tht express is aware that it's behind a proxy of ingress-nginx and still trust it
+app.set('trust proxy', true);
+app.use(json());
+app.use(
+    cookieSession({
+        signed: false,
+        secure: process.env.NODE_ENV !== 'test'
+    }));
+
+app.use(currentUser);
+
+app.use(createTicketRouter);
+app.use(showTicketRouter);
+app.use(indexTicketRouter);
+app.use(updateTicket);
+
+app.all('*', async (req: Request, res: Response) => {
+    throw new NotFoundError();
+});
+
+app.use(errorHandler);
+
+export { app };
