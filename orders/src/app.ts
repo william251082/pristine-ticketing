1f38a7ef70dc64cb537b80e7d469dd628afb53a4
william <william.del.rosario@iceshop.nl>
2020-07-29 09:05:40 +0200

    Scaffolding a Few Route Handlers

--- orders/src/app.ts
+++ orders/src/app.ts
@@ -4,11 +4,10 @@ import { json } from 'body-parser';
 import cookieSession from 'cookie-session';
 
 import { errorHandler, NotFoundError, currentUser } from "@iceshoptickets/common";
-
-import {createTicketRouter} from "./routes/new";
-import {showTicketRouter} from "./routes/show";
-import {indexTicketRouter} from "./routes";
-import {updateTicket} from "./routes/update";
+import {indexOrderRouter} from "./routes";
+import {newOrderRouter} from "./routes/new";
+import {showOrderRouter} from "./routes/show";
+import {deleteOrderRouter} from "./routes/delete";
 
 const app = express();
 // make sure tht express is aware that it's behind a proxy of ingress-nginx and still trust it
@@ -22,10 +21,10 @@ app.use(
 
 app.use(currentUser);
 
-app.use(createTicketRouter);
-app.use(showTicketRouter);
-app.use(indexTicketRouter);
-app.use(updateTicket);
+app.use(indexOrderRouter);
+app.use(newOrderRouter);
+app.use(showOrderRouter);
+app.use(deleteOrderRouter);
 
 app.all('*', async (req: Request, res: Response) => {
     throw new NotFoundError();
