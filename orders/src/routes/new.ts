aeaf7cfb4db9c0a56264d915e1c3d0d83ae6f56f
william <william.del.rosario@iceshop.nl>
2020-07-30 15:28:53 +0200

    Convenience Document Methods

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -4,6 +4,7 @@ import mongoose from "mongoose";
 import {body} from "express-validator";
 import {Ticket} from "../models/ticket";
 import {Order} from "../models/order";
+import {is} from "@babel/types";
 
 const router = express.Router();
 
@@ -27,22 +28,12 @@ router.post('/api/orders', requireAuth,
 
     // Make sure the ticket is not already reserved
     // Run query to look at all orders.
-    // Find an order where the ticket is the ticket we just found *and* theorders status is *not cancelled.*
-    // If we find an irder from that means the ticket *is reserved*
-    const existingOrder = await Order.findOne({
-        ticket: ticket,
-        status: {
-            $in: [
-                OrderStatus.Created,
-                OrderStatus.AwaitingPayment,
-                OrderStatus.Complete
-            ]
-        }
-    });
-    if (existingOrder) {
+    // Find an order where the ticket is the ticket we just found *and* the orders status is *not cancelled.*
+    // If we find an order from that means the ticket *is reserved*
+    const isReserved = await ticket.isReserved()
+;    if (isReserved) {
         throw new BadRequestError('Ticket is already reserved');
     }
-
     // Calculate an expiration date for this order
 
     // Build the order and save it to the database
