573373672f3fe1439568b3b2a1986730811f8d66
william <william.del.rosario@iceshop.nl>
2020-07-30 14:36:18 +0200

    More on Mongoose Refs

---
+++ orders/src/models/order.bak.ts
@@ -0,0 +1,30 @@
+import {Ticket} from "../../../tickets/src/models/ticket";
+import {OrderStatus} from "@iceshoptickets/common";
+import {Order} from "./orders";
+
+// @ts-nocheck
+// To associate an existing Order and Ticket together
+const ticket = await Ticket.findOne({});
+const order = await Order.findOne({});
+
+order.ticket = ticket;
+await order.save();
+
+
+// To associate an existing Ticket with a *new* Order
+const ticket = await Ticket.findOne({});
+const order = Order.build({
+    ticket: ticket,
+    userId: '...',
+    status: OrderStatus.Created,
+    ticket: tomorrow,
+});
+
+order.ticket = ticket;
+await order.save();
+
+
+// To fetch an existing Order from the db with its associated Ticket
+const order = await Order.findById('...').populate('ticket');
+order.ticket.title
+order.ticket.price
