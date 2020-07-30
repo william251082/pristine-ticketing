25311c1a82192d1e64f35f82ef17a59abd725bf2
william <william.del.rosario@iceshop.nl>
2020-07-30 15:34:30 +0200

    Order Expiration Times

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -4,9 +4,10 @@ import mongoose from "mongoose";
 import {body} from "express-validator";
 import {Ticket} from "../models/ticket";
 import {Order} from "../models/order";
-import {is} from "@babel/types";
 
 const router = express.Router();
+
+const EXPIRATION_WINDOW_SECONDS = 15 * 60;
 
 router.post('/api/orders', requireAuth,
     [
@@ -34,13 +35,23 @@ router.post('/api/orders', requireAuth,
 ;    if (isReserved) {
         throw new BadRequestError('Ticket is already reserved');
     }
+
     // Calculate an expiration date for this order
+    const expiration = new Date();
+    expiration.setSeconds(expiration.getSeconds() + EXPIRATION_WINDOW_SECONDS);
 
     // Build the order and save it to the database
+    const order = Order.build({
+        userId: req.currentUser!.id,
+        status: OrderStatus.Created,
+        expiresAt: expiration,
+        ticket
+    });
+    await order.save();
 
     // Publish an event saying that an order was created
 
-    res.send({});
+    res.send(201).send(order);
 });
 
 export { router as newOrderRouter };
