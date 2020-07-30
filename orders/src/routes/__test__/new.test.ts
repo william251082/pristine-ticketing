04cb4f60691876c88f7cb355f198388534a6fc41
william <william.del.rosario@iceshop.nl>
2020-07-30 16:15:34 +0200

    Asserting Reserved Tickets

--- orders/src/routes/__test__/new.test.ts
+++ orders/src/routes/__test__/new.test.ts
@@ -1,6 +1,9 @@
 import request = require("supertest");
 import {app} from "../../app";
 import mongoose from "mongoose";
+import {Ticket} from "../../models/ticket";
+import {Order} from "../../models/order";
+import {OrderStatus} from "@iceshoptickets/common";
 
 it('returns an error if the ticket does not exist', async () => {
     const ticketId = mongoose.Types.ObjectId();
@@ -13,6 +16,24 @@ it('returns an error if the ticket does not exist', async () => {
 });
 
 it('returns an error if the ticket is already reserved', async () => {
+    const ticket = Ticket.build({
+        title: 'concert',
+        price: 20
+    });
+    await ticket.save();
+    const order = Order.build({
+        ticket,
+        userId: 'rethfg',
+        status: OrderStatus.Created,
+        expiresAt: new Date()
+    });
+    await order.save();
+
+    await request(app)
+        .post('/api/orders')
+        .set('Cookie', global.signin())
+        .send({ ticketId: ticket.id })
+        .expect(400)
 
 });
 
