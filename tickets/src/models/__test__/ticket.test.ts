6b618305b76a5f8262682052a5997cc7065c5167
william <william.del.rosario@iceshop.nl>
2020-07-31 10:17:23 +0200

    Implementing OCC with Mongoose

---
+++ tickets/src/models/__test__/ticket.test.ts
@@ -0,0 +1,3 @@
+it('implements optimistic currency control', async () => {
+
+});
--- tickets/src/models/ticket.ts
+++ tickets/src/models/ticket.ts
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import {updateIfCurrentPlugin} from "mongoose-update-if-current";
 
 // describes required props of new User
 interface TicketAttrs {
@@ -17,6 +18,7 @@ interface TicketDoc extends mongoose.Document {
     title: string;
     price: number;
     userId: string;
+    version: number;
 }
 
 const ticketSchema = new mongoose.Schema({
@@ -42,6 +44,9 @@ const ticketSchema = new mongoose.Schema({
         }
     }
 });
+
+ticketSchema.set('versionKey', 'version');
+ticketSchema.plugin(updateIfCurrentPlugin);
 
 ticketSchema.statics.build = (attrs: TicketAttrs) => {
     return new Ticket(attrs);
