47d1f6e30e5333c6a58f80f5621fd1da5247ff06
william <william.del.rosario@iceshop.nl>
2020-08-01 16:43:20 +0200

    Private vs Protected Properties

--- tickets/src/events/listeners/order-created-listener.ts
+++ tickets/src/events/listeners/order-created-listener.ts
@@ -2,6 +2,8 @@ import {Listener, OrderCreatedEvent, Subjects} from "@iceshoptickets/common";
 import {queueGroupName} from "./queue-group-name";
 import {Message} from 'node-nats-streaming';
 import {Ticket} from "../../models/ticket";
+import {TicketUpdatedPublisher} from "../publishers/ticket-updated-publisher";
+import {natsWrapper} from "../../nats-wrapper";
 
 export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
     subject: Subjects.OrderCreated = Subjects.OrderCreated;
@@ -21,6 +23,7 @@ export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
 
         // save the ticket
         await ticket.save();
+        new TicketUpdatedPublisher(natsWrapper.client);
 
         // ack the message
         msg.ack();
