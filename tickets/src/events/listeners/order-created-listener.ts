a4d9d46f7d5f2e3642c77ebc36198eeeae721c4e
william <william.del.rosario@iceshop.nl>
2020-08-01 14:46:02 +0200

    Building the Listener

---
+++ tickets/src/events/listeners/order-created-listener.ts
@@ -0,0 +1,10 @@
+import {Listener, OrderCreatedEvent, Subjects} from "@iceshoptickets/common";
+import {queueGroupName} from "./queue-group-name";
+import {Message} from 'node-nats-streaming';
+
+export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
+    subject: Subjects.OrderCreated = Subjects.OrderCreated;
+    queueGroupNmae = queueGroupName;
+
+    async onMessage(data: OrderCreatedEvent['data'], msg: Message)
+}
