3393cb60425ec02a640d3a9d530a108860ccc8c1
william <william.del.rosario@iceshop.nl>
2020-08-01 16:52:35 +0200

    Publishing While Listening

--- tickets/src/events/listeners/order-created-listener.ts
+++ tickets/src/events/listeners/order-created-listener.ts
@@ -3,7 +3,6 @@ import {queueGroupName} from "./queue-group-name";
 import {Message} from 'node-nats-streaming';
 import {Ticket} from "../../models/ticket";
 import {TicketUpdatedPublisher} from "../publishers/ticket-updated-publisher";
-import {natsWrapper} from "../../nats-wrapper";
 
 export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
     subject: Subjects.OrderCreated = Subjects.OrderCreated;
@@ -23,7 +22,14 @@ export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
 
         // save the ticket
         await ticket.save();
-        new TicketUpdatedPublisher(natsWrapper.client);
+        await new TicketUpdatedPublisher(this.client).publish({
+            id: ticket.id,
+            price: ticket.price,
+            title: ticket.title,
+            userId: ticket.userId,
+            orderId: ticket.orderId,
+            version: ticket.version
+        });
 
         // ack the message
         msg.ack();
