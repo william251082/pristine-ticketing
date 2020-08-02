fb7ff51e6aa734fb6b037059d23e33a0b2d81654
william <william.del.rosario@iceshop.nl>
2020-08-02 14:18:08 +0200

    Replicating Orders

---
+++ payments/src/events/listeners/order-created-listener.ts
@@ -0,0 +1,22 @@
+import { Message } from 'node-nats-streaming';
+import { queueGroupName } from './queue-group-name';
+import { Order } from '../../models/order';
+import {Listener, OrderCreatedEvent, Subjects} from "@iceshoptickets/common";
+
+export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
+  subject: Subjects.OrderCreated = Subjects.OrderCreated;
+  queueGroupName = queueGroupName;
+
+  async onMessage(data: OrderCreatedEvent['data'], msg: Message) {
+    const order = Order.build({
+      id: data.id,
+      price: data.ticket.price,
+      status: data.status,
+      userId: data.userId,
+      version: data.version,
+    });
+    await order.save();
+
+    msg.ack();
+  }
+}
