a5678f42ea8ee4d3f3bf2fbdce348ab9fe328f1d
william <william.del.rosario@iceshop.nl>
2020-08-02 16:17:50 +0200

    Publishing a Payment Created Event

--- payments/src/events/publishers/payment-created-publisher.ts
+++ payments/src/events/publishers/payment-created-publisher.ts
@@ -1,4 +1,4 @@
-import { Subjects, Publisher, PaymentCreatedEvent } from '@sgtickets/common';
+import { Subjects, Publisher, PaymentCreatedEvent } from '@iceshoptickets/common';
 
 export class PaymentCreatedPublisher extends Publisher<PaymentCreatedEvent> {
   subject: Subjects.PaymentCreated = Subjects.PaymentCreated;
