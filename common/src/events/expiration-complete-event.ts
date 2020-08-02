9fbdb986c812b6e521df379d547a670c434e764c
william <william.del.rosario@iceshop.nl>
2020-08-02 11:17:13 +0200

    Updates

--- common/src/events/expiration-complete-event.ts
+++ common/src/events/expiration-complete-event.ts
@@ -3,6 +3,6 @@ import {Subjects} from "./subjects";
 export interface ExpirationCompleteEvent {
     subject: Subjects.ExpirationComplete;
     data: {
-        odderId: string;
+        orderId: string;
     };
 }
