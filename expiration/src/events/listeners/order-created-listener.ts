13abb485dbaff7c830e63b36658e85a8a735f4b1
william <william.del.rosario@iceshop.nl>
2020-08-02 13:38:19 +0200

    Don't Cancel Completed Orders!

--- expiration/src/events/listeners/order-created-listener.ts
+++ expiration/src/events/listeners/order-created-listener.ts
@@ -14,9 +14,9 @@ export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
         await expirationQueue.add({
             orderId: data.id
         },
-            {
-                delay
-            }
+            // {
+            //     delay
+            // }
         );
 
         msg.ack();
