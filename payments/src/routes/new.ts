62b9b75d35932d98d95d83f1cf4494f59036e375
william <william.del.rosario@iceshop.nl>
2020-08-02 14:44:12 +0200

    Validating Order Payment

--- payments/src/routes/new.ts
+++ payments/src/routes/new.ts
@@ -1,9 +1,13 @@
 import express, { Request, Response } from 'express';
 import { body } from 'express-validator';
 import {
+    BadRequestError,
+    NotAuthorizedError,
+    NotFoundError, OrderStatus,
     requireAuth,
     validateRequest
 } from "@iceshoptickets/common";
+import {Order} from "../models/order";
 
 const router = express.Router();
 
@@ -13,6 +17,20 @@ router.post(
   [body('token').not().isEmpty(), body('orderId').not().isEmpty()],
   validateRequest,
   async (req: Request, res: Response) => {
+    const { token, orderId } = req.body;
+
+    const order = await Order.findById(orderId);
+
+    if (!order) {
+      throw new NotFoundError();
+    }
+    if (order.userId !== req.currentUser!.id) {
+      throw new NotAuthorizedError();
+    }
+    if (order.status === OrderStatus.Cancelled) {
+      throw new BadRequestError('Cannot pay for an cancelled order');
+    }
+
     res.send({ success: true });
   }
 );
