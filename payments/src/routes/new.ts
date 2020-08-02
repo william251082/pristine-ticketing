c499895ea86fc7073be254388eac710382fba3e7
william <william.del.rosario@iceshop.nl>
2020-08-02 16:21:06 +0200

    More on Publishing

--- payments/src/routes/new.ts
+++ payments/src/routes/new.ts
@@ -11,6 +11,8 @@ import {
     requireAuth,
     validateRequest
 } from "@iceshoptickets/common";
+import {PaymentCreatedPublisher} from "../events/publishers/payment-created-publisher";
+import {natsWrapper} from "../nats-wrapper";
 
 const router = express.Router();
 
@@ -44,6 +46,11 @@ router.post(
       stripeId: charge.id,
     });
     await payment.save();
+    new PaymentCreatedPublisher(natsWrapper.client).publish({
+      id: payment.id,
+      orderId: payment.orderId,
+      stripeId: payment.stripeId,
+    });
 
     res.status(201).send({ id: payment.id });
   }
