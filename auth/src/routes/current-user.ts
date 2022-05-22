f988dd5b8e0eb859322e8f4405df0938eef60a6f
william <william@magicmanager.nl>
2022-05-22 20:49:57 +0200

    Returning the Current User

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,10 +1,11 @@
 import express, { Request, Response } from 'express';
+import { currentUser } from '../middlewares/current-user';
 // import {currentUser} from "@iceshoptickets/common";
 
 const router = express.Router();
 
-router.get('/api/users/currentuser', (req: Request, res: Response ) => {
-    res.send('hi')
+router.get('/api/users/currentuser', currentUser, (req: Request, res: Response ) => {
+    res.send(req.currentUser)
 })
 
 export { router as currentUserRouter }
