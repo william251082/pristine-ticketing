3c264c6c30bff9ac8630f9f1c7091b90f5906c0a
william <william@magicmanager.nl>
2022-05-22 21:26:15 +0200

    Index to App Refactor

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,17 +1,5 @@
 import mongoose from "mongoose";
-import { json } from 'body-parser';
-import 'express-async-errors'
-import cookieSession from 'cookie-session';
-
-import express, {Request, Response} from 'express';
-import { currentUserRouter } from "./routes/current-user";
-import { signinRouter } from "./routes/signin";
-import { signupRouter } from "./routes/signup";
-import { signoutRouter } from "./routes/signout";
-import { NextFunction } from "express";
-import { NotFoundError } from "./errors/not-found-error";
-import { errorHandler } from "./middlewares/error-handler";
-
+import { app } from "./app";
 
 const start = async () => {
     console.log('Starting up...')
@@ -29,27 +17,12 @@ const start = async () => {
       console.error(err)
   }
 };
-const app = express()
-app.set('trust proxy', true)
-app.use(json())
-app.use(
-    cookieSession({
-        signed: false,
-        secure: true
-    })
-)
-app.use(currentUserRouter)
-app.use(signinRouter)
-app.use(signupRouter)
-app.use(signoutRouter)
-app.get('*', async (req: Request, res: Response, next: NextFunction) => {
-    throw new NotFoundError()
-})
-app.use(errorHandler)
-start();
+
 app.listen(3000, () => {
     console.log('Listening on port 3000, auth')
 });
+
+start();
 
 
 
