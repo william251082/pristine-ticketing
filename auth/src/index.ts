4bbe8de6b2a1866f90841abe08fe9a50fc6d6741
william <william@magicmanager.nl>
2022-05-22 19:35:17 +0200

    Adding Session Support

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 import { json } from 'body-parser';
 import 'express-async-errors'
+import cookieSession from 'cookie-session';
 
 import express, {Request, Response} from 'express';
 import { currentUserRouter } from "./routes/current-user";
@@ -30,7 +31,14 @@ const start = async () => {
   }
 };
 const app = express()
+app.set('trust proxy', true)
 app.use(json())
+app.use(
+    cookieSession({
+        signed: false,
+        secure: true
+    })
+)
 app.use(currentUserRouter)
 app.use(signinRouter)
 app.use(signupRouter)
