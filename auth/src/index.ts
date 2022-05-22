1ad025f442d5cbc65316711fa9e417f3f0273560
william <william@magicmanager.nl>
2022-05-22 13:24:32 +0200

    async error handling in express

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
 import { json } from 'body-parser';
+import 'express-async-errors'
 
 import express, {Request, Response} from 'express';
 import { currentUserRouter } from "./routes/current-user";
@@ -7,6 +8,8 @@ import { signinRouter } from "./routes/signin";
 import { signupRouter } from "./routes/signup";
 import { signoutRouter } from "./routes/signout";
 import { errorHandler } from "./middlewares/error-handler";
+import { NotFoundError } from "./middlewares/not-found-error";
+import { NextFunction } from "express";
 
 
 const start = async () => {
@@ -29,13 +32,19 @@ const start = async () => {
   //     console.error(err)
   // }
 };
-const app = express();
-app.use(json());
-app.use(currentUserRouter);
-app.use(signinRouter);
-app.use(signupRouter);
-app.use(signoutRouter);
-app.use(errorHandler);
+const app = express()
+app.use(json())
+app.use(currentUserRouter)
+app.use(signinRouter)
+app.use(signupRouter)
+app.use(signoutRouter)
+// app.get('*', async (req: Request, res: Response, next: NextFunction) => {
+//     next(new NotFoundError())
+// })
+app.get('*', async (req: Request, res: Response, next: NextFunction) => {
+    throw new NotFoundError()
+})
+app.use(errorHandler)
 app.listen(3000, () => {
     console.log('Listening on port 3000, auth')
 });
