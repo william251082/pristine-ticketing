3e41dd54ab939fffd53ce5f1f1da1af8f6961556
william <william@magicmanager.nl>
2022-05-23 12:38:26 +0200

    Tickets api init

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -1,5 +1,4 @@
 import mongoose from "mongoose";
-
 import { app } from './app';
 import {natsWrapper} from "./nats-wrapper";
 import {OrderCreatedListener} from "./events/listeners/order-created-listener";
@@ -12,36 +11,32 @@ const start = async () => {
     if (!process.env.MONGO_URI) {
         throw new Error('MONGO_URI must be defined');
     }
-    if (!process.env.NATS_CLIENT_ID) {
-        throw new Error('NATS_CLIENT_ID must be defined');
-    }
-    if (!process.env.NATS_URL) {
-        throw new Error('NATS_URL must be defined');
-    }
-    if (!process.env.NATS_CLUSTER_ID) {
-        throw new Error('NATS_CLUSTER_ID must be defined');
-    }
+    // if (!process.env.NATS_CLIENT_ID) {
+    //     throw new Error('NATS_CLIENT_ID must be defined');
+    // }
+    // if (!process.env.NATS_URL) {
+    //     throw new Error('NATS_URL must be defined');
+    // }
+    // if (!process.env.NATS_CLUSTER_ID) {
+    //     throw new Error('NATS_CLUSTER_ID must be defined');
+    // }
   try {
-      await natsWrapper.connect(
-          process.env.NATS_CLUSTER_ID,
-          process.env.NATS_CLIENT_ID,
-          process.env.NATS_URL
-      );
-        natsWrapper.client.on('close', () => {
-            console.log('NATS connection closed!');
-            process.exit();
-        });
-        process.on('SIGINT', () => natsWrapper.client.close());
-        process.on('SIGTERM', () =>natsWrapper.client.close());
+      // await natsWrapper.connect(
+      //     process.env.NATS_CLUSTER_ID,
+      //     process.env.NATS_CLIENT_ID,
+      //     process.env.NATS_URL
+      // );
+      //   natsWrapper.client.on('close', () => {
+      //       console.log('NATS connection closed!');
+      //       process.exit();
+      //   });
+      //   process.on('SIGINT', () => natsWrapper.client.close());
+      //   process.on('SIGTERM', () =>natsWrapper.client.close());
+      //
+      // new OrderCreatedListener(natsWrapper.client).listen();
+      // new OrderCancelledListener(natsWrapper.client).listen();
 
-      new OrderCreatedListener(natsWrapper.client).listen();
-      new OrderCancelledListener(natsWrapper.client).listen();
-
-      await mongoose.connect(process.env.MONGO_URI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useCreateIndex: true
-    });
+      await mongoose.connect(process.env.MONGO_URI);
       console.log('Connected to Mongodb');
   } catch (err) {
       console.error(err)
@@ -49,7 +44,7 @@ const start = async () => {
 };
 
 app.listen(3000, () => {
-    console.log('Listening on port 3000, auth')
+    console.log('Listening on port 3000, tickets')
 });
 
 start();
