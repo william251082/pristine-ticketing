d2b5fe12104d9bf3e22d52373552951c9e356cc5
william <william@magicmanager.nl>
2022-05-23 12:51:22 +0200

    Auth Update

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -4,15 +4,15 @@ import { app } from "./app";
 const start = async () => {
     console.log('Starting up...')
     if (!process.env.JWT_KEY) {
-        throw new Error('JWT_KEY must be defined');
+        throw new Error('JWT_KEY must be defined')
     }
     if (!process.env.MONGO_URI) {
-        throw new Error('MONGO_URI must be defined');
+        throw new Error('MONGO_URI must be defined')
     }
 
   try {
       await mongoose.connect(process.env.MONGO_URI);
-      console.log('Connected to Mongodb');
+      console.log('Connected to Mongodb')
   } catch (err) {
       console.error(err)
   }
@@ -22,7 +22,7 @@ app.listen(3000, () => {
     console.log('Listening on port 3000, auth')
 });
 
-start();
+start()
 
 
 
--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -6,10 +6,10 @@ import {OrderCancelledListener} from "./events/listeners/order-cancelled-listene
 
 const start = async () => {
     if (!process.env.JWT_KEY) {
-        throw new Error('JWT_KEY must be defined');
+        throw new Error('JWT_KEY must be defined')
     }
     if (!process.env.MONGO_URI) {
-        throw new Error('MONGO_URI must be defined');
+        throw new Error('MONGO_URI must be defined')
     }
     // if (!process.env.NATS_CLIENT_ID) {
     //     throw new Error('NATS_CLIENT_ID must be defined');
@@ -36,8 +36,12 @@ const start = async () => {
       // new OrderCreatedListener(natsWrapper.client).listen();
       // new OrderCancelledListener(natsWrapper.client).listen();
 
-      await mongoose.connect(process.env.MONGO_URI);
-      console.log('Connected to Mongodb');
+      await mongoose.connect(process.env.MONGO_URI, {
+          useNewUrlParser: true,
+          useUnifiedTopology: true,
+          useCreateIndex: true
+      })
+      console.log('Connected to Mongodb')
   } catch (err) {
       console.error(err)
   }
@@ -45,6 +49,6 @@ const start = async () => {
 
 app.listen(3000, () => {
     console.log('Listening on port 3000, tickets')
-});
+})
 
-start();
+start()
